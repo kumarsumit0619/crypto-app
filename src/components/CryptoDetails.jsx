91c4c38772ce5a7ad06c0f38c2247efbcf135513
Sumit Kumar <kumar.sumit0619@gmail.com>
2024-09-01 00:34:00 +0530

    trial101

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const CryptoDetails = () => {
-  return <div> fdgfdghdg </div>;
+  return (
+    <div> CryptoDetails................................................. </div>
+  );
 };
 
 export default CryptoDetails;
